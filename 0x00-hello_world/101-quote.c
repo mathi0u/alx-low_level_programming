4126e197fea72f295718d0aea29c27d4c88f6683
Mathi0u <saremakhtar@gmail.com>
2022-12-09 06:09:38 -0800

    d

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
-    #include <unistd.h>
+#include <unistd.h>
 
-    int main(void)
-    {
-	    write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-	    return (1);
-    }
+/**
+ * main - print and that piece of art is useful
+ * Return: return error(s)
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 
+
+/**
+ * main - print Programming is like building a multilingual puzzle
+ * Return: 0 if no error(s)
+ */
 int main(void)
-    {
-        puts("\"Programming is like building a multilingual puzzle");
+{
+	puts("\"Programming is like building a multilingual puzzle");
         return 0;
-    }
+}
